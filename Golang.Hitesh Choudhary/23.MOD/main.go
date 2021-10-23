d150b8052b78aa7070c79b9a1b02f7314171cf65
theMillenniumFalcon <nishankpr2002@gmail.com>
2021-10-23 22:06:55 +0530

    MOD-added

--- Golang.Hitesh Choudhary/23.MOD/main.go
+++ Golang.Hitesh Choudhary/23.MOD/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	fmt.Println("MOD in go")
+	greeter()
+	r := mux.NewRouter()
+	r.HandleFunc("/", serveHome).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":4000", r))
+}
+
+func greeter() {
+	fmt.Println("Hey there mod users")
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<html>Welcome to golang!</html>"))
 }
